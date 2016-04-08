9cb29aac49e26c6ddf2ea6b2b37fccbfcfca8ccb
jaslatendresse <jlatendresse.39@gmail.com>
2016-04-08 14:19:36 -0400

    changed layout of home and removed pic of the day tab

--- script.js
+++ script.js
@@ -1,56 +1,57 @@
 function showRemaining() {
-            var end = new Date('09/10/2016 9:00 PM');
-            var _second = 1000;
-            var _minute = _second * 60;
-            var _hour = _minute * 60;
-            var _day = _hour * 24;
-            var timer;
-            var now = new Date();
-            var distance = end - now;
-            if (distance < 0) {
+    "use strict";
+    var end = new Date('09/10/2016 9:00 PM'),
+        second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24,
+        timer,
+        now = new Date(),
+        distance = end - now;
+    
+    if (distance < 0) {
+        clearInterval(timer);
+        document.getElementById('countdown').innerHTML = 'EXPIRED!';
 
-                clearInterval(timer);
-                document.getElementById('countdown').innerHTML = 'EXPIRED!';
-
-            return;
-            }
-        var days = Math.floor(distance / _day);
-        var hours = Math.floor((distance % _day) / _hour);
-        var minutes = Math.floor((distance % _hour) / _minute);
-        var seconds = Math.floor((distance % _minute) / _second);
-
-        document.getElementById('countdown').innerHTML = days + 'days ';
-        document.getElementById('countdown').innerHTML += hours + 'hrs ';
-        document.getElementById('countdown').innerHTML += minutes + 'mins ';
-        document.getElementById('countdown').innerHTML += seconds + 'secs';
+        return;
     }
-    timer = setInterval(showRemaining, 1000);
+    var days = Math.floor(distance / day),
+        hours = Math.floor((distance % day) / hour),
+        minutes = Math.floor((distance % hour) / minute),
+        seconds = Math.floor((distance % minute) / second);
+
+    document.getElementById('countdown').innerHTML = days + 'days ';
+    document.getElementById('countdown').innerHTML += hours + 'hrs ';
+    document.getElementById('countdown').innerHTML += minutes + 'mins ';
+    document.getElementById('countdown').innerHTML += seconds + 'secs';
+}
+timer = setInterval(showRemaining, 1000);
 
 
-        function displayDate(){
-            var d = new Date(),
-            year = d.getFullYear(),
-            months = ['January','Febuary','March','April','May','June','July','August','September','October','November','December'],
-            days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+function displayDate() {
+    "use strict";
+    var d = new Date(),
+        year = d.getFullYear(),
+        months = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-            document.getElementById("date").innerHTML = months[d.getMonth()]+' '+d.getDate()+', '+year;
-        }
+    document.getElementById("date").innerHTML = months[d.getMonth()] + ' ' + d.getDate() + ', ' + year;
+}
 
+var dailyPhotos, today, img;
 
-        var dailyPhotos;
-        var today, img;
+    dailyPhotos = function () {
+    "use strict";
+    today = new Date();
+    weekday = today.getDay();
+    showImages = [];
+    myPhotos = ["./pic5.jpg", "./pic6.png", "./pic1.png", "./pic2.png", "./pic3.png", "./pic4.png"];
 
-        dailyPhotos = function(){
-            today = new Date();
-            weekday = today.getDay();
-            showImages = [];
-            myPhotos = ["./pic5.jpg", "./pic6.png","./pic1.png","./pic2.png","./pic3.png","./pic4.png"];
-
-            if(document.images) {
-                for(var i = 0; i<myPhotos.length; i++){
-                    showImages[i] = new Image();
-                    showImages[i].src = myPhotos[i];
-                } img = ((document.getElementById) ? document.getElementById("picoftheday"): document.images.picoftheday);
+        if(document.images) {
+            for(var i = 0; i<myPhotos.length; i++){
+                showImages[i] = new Image();
+                showImages[i].src = myPhotos[i];
+            } img = ((document.getElementById) ? document.getElementById("picoftheday"): document.images.picoftheday);
                          img.src = showImages[weekday].src;
                          img.alt = myPhotos[weekday];
             }return false;
